1092069d3a089fefe35581ed236e91791ac39735
bigcouchman <69228834+bigcouchman@users.noreply.github.com>
2020-09-14 19:22:28 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var tree;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+tree = new Tree(600,400,50,300);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  tree.display();
+
+  drawSprites();
+ 
+}
+
+
+
